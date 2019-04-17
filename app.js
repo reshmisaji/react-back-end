8897284f481dbdf4a365b48b9ae78e83b5d8f145
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2019-04-17 15:50:44 +0530

    Removed the mysql database.

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const Express = require("express");
-const mysql = require("mysql");
+// const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "user",
-  password: "user",
-  database: "practice"
-});
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "user",
+//   password: "user",
+//   database: "practice"
+// });
 
-connection.connect(err => console.log(err));
+// connection.connect(err => console.log(err));
 
 const app = new Express();
 
 const renderHomePage = function(req, res) {
-  connection.query("select * from flowers;", (error, data, fields) => {
-    res.send(JSON.stringify(data));
-  });
+  // connection.query("select * from flowers;", (error, data, fields) => {
+    // console.log(data);
+    res.send(JSON.stringify([{ id: 1, flower_name: 'Rose', color: 'Red' }]));
+  // });
 };
 
 const PORT = process.env.PORT || 8080;
